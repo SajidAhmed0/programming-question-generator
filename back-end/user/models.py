9c5696955f974ff015c8ecf3d9306ee8538acf58
SajidAhmed0 <mjsahmed205@gmail.com>
2025-04-24 14:45:21 +0530

    feat(back-end): update user model

--- back-end/user/models.py
+++ back-end/user/models.py
@@ -5,6 +5,13 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.TextField()
     name = models.CharField(max_length=255)
+    average = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0.00,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.email
