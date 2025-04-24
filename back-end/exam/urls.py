81a9c9ad34d61fe0c72047a1e82d65082614b908
SajidAhmed0 <mjsahmed205@gmail.com>
2025-04-24 19:53:57 +0530

    cors(back-end): fix syntax error

--- back-end/exam/urls.py
+++ back-end/exam/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ExamView, ExamAnswerView
 
 urlpatterns = [
-    path('programming/<int:user_id>/exams/', ExamView.as_view(), name='exam-view')
+    path('programming/<int:user_id>/exams/', ExamView.as_view(), name='exam-view'),
     path('programming/<int:user_id>/exams/<int:exam_id>/answers/', ExamAnswerView.as_view(), name='exam-anser-view')
 ]
